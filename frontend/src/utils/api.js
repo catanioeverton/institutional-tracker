5fbd304c8a392206afe8421e8ac4638bb35e85fe
catanioeverton <catanioeverton@gmail.com>
2026-02-04 14:41:48 -0300

    Conectando API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // URL DO BACKEND NA VERCEL
 // (Quando você subir o backend, copie a URL dele e cole aqui no lugar deste texto)
-const PROD_URL = 'https://sua-url-backend-na-vercel.app';
+const PROD_URL = 'https://institutional-tracker-backend.vercel.app';
 
 const api = axios.create({
     baseURL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
