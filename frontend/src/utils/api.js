d613e2de7a2a1d3212d25fcadbd0099aace67c46
catanioeverton <catanioeverton@gmail.com>
2026-02-04 15:27:26 -0300

    teste login

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-// URL FIXA DA VERCEL (Copie exatamente o link do seu Backend em 'Domains' na Vercel)
-// Exemplo: 'https://institutional-tracker-backend.vercel.app'
-const PROD_URL = 'https://institutional-tracker-backend.vercel.app';
-
+// OBRIGATÓRIO: Link do BACKEND (A Cozinha/API)
+// Se colocar o do frontend aqui, não funciona.
 const api = axios.create({
-    // FORÇAMOS usar sempre a URL da produção, mesmo estando local.
-    // Isso garante que se funcionar no seu PC, TEM que funcionar no celular.
-    baseURL: `${PROD_URL}/api`
+    baseURL: 'https://institutional-tracker-backend.vercel.app/api'
 });
 
 export default api;
